233428ca76da24d511e74f3afacf77bffc5e3295
joymohanta <joymohanta06@gmail.com>
2022-02-17 02:10:35 +0600

    almost done

--- js/money.js
+++ js/money.js
@@ -1,12 +1,22 @@
-document.getElementById("calculate-btn").addEventListener("click", function () {
+function calculation() {
   const paymentInput = document.getElementById("payment");
   const payment = parseFloat(paymentInput.value);
   const balance = document.getElementById("balance");
   const balanceNumber = parseFloat(balance.innerText);
   const totalBalance = payment;
-  balance.innerText = totalBalance;
-});
+  balance.innerText = totalBalance - calculationExpense();
+}
+
 document.getElementById("calculate-btn").addEventListener("click", function () {
+  calculation();
+  /* const paymentInput = document.getElementById("payment");
+  const payment = parseFloat(paymentInput.value);
+  const balance = document.getElementById("balance");
+  const balanceNumber = parseFloat(balance.innerText);
+  const totalBalance = payment;
+  balance.innerText = totalBalance; */
+});
+function calculationExpense() {
   const foodInput = document.getElementById("food");
   const food = parseFloat(foodInput.value);
   const rentInput = document.getElementById("rent");
@@ -18,7 +28,24 @@ document.getElementById("calculate-btn").addEventListener("click", function () {
   const expenseNumber = parseFloat(expense.innerText);
   const totalExpense = food + rent + cloth;
   expense.innerText = totalExpense;
+  return totalExpense;
+}
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  calculationExpense();
+  // console.log(calculationExpense());
+  /* const foodInput = document.getElementById("food");
+  const food = parseFloat(foodInput.value);
+  const rentInput = document.getElementById("rent");
+  const rent = parseFloat(rentInput.value);
+  const clothInput = document.getElementById("cloth");
+  const cloth = parseFloat(clothInput.value);
+
+  const expense = document.getElementById("expense");
+  const expenseNumber = parseFloat(expense.innerText);
+  const totalExpense = food + rent + cloth;
+  expense.innerText = totalExpense; */
 });
+//saving part
 document.getElementById("save-btn").addEventListener("click", function () {
   const saveInput = document.getElementById("percent-input");
   const inputNumber = parseFloat(saveInput.value);
