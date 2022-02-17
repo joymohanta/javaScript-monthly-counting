0a4390350b4048fdf911a243a7bc2ba2f366c2b5
joymohanta <joymohanta06@gmail.com>
2022-02-17 17:50:52 +0600

    finally done

--- js/money.js
+++ js/money.js
@@ -1,3 +1,4 @@
+// Balance portion
 function calculation() {
   const paymentInput = document.getElementById("payment");
   const payment = parseFloat(paymentInput.value);
@@ -7,20 +8,18 @@ function calculation() {
   balance.innerText = totalBalance - calculationExpense();
   return balance.innerText;
 }
-
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const paymentInput = document.getElementById("payment");
-  const payment = parseFloat(paymentInput.value);
-  if (payment > 0) {
+  const paymentInput = document.getElementById("payment").value;
+  if (paymentInput >= 0) {
     calculation();
+    const error = document.getElementById("error");
+    error.style.display = "none";
+  } else {
+    const error = document.getElementById("error");
+    error.style.display = "block";
   }
-  /* const paymentInput = document.getElementById("payment");
-  const payment = parseFloat(paymentInput.value);
-  const balance = document.getElementById("balance");
-  const balanceNumber = parseFloat(balance.innerText);
-  const totalBalance = payment;
-  balance.innerText = totalBalance; */
 });
+//Expenses Portion
 function calculationExpense() {
   const foodInput = document.getElementById("food");
   const food = parseFloat(foodInput.value);
@@ -35,34 +34,23 @@ function calculationExpense() {
   return totalExpense;
 }
 document.getElementById("calculate-btn").addEventListener("click", function () {
-  const foodInput = document.getElementById("food");
-  const food = parseFloat(foodInput.value);
-  const rentInput = document.getElementById("rent");
-  const rent = parseFloat(rentInput.value);
-  const clothInput = document.getElementById("cloth");
-  const cloth = parseFloat(clothInput.value);
-  if (food > 0 || rent > 0 || cloth > 0) {
+  const foodInput = document.getElementById("food").value;
+  const rentInput = document.getElementById("rent").value;
+  const clothInput = document.getElementById("cloth").value;
+  if (foodInput >= 0 && rentInput >= 0 && clothInput >= 0) {
     calculationExpense();
+    const error = document.getElementById("error");
+    error.style.display = "none";
+  } else {
+    const error = document.getElementById("error");
+    error.style.display = "block";
   }
-
-  // console.log(calculationExpense());
-  /* const foodInput = document.getElementById("food");
-  const food = parseFloat(foodInput.value);
-  const rentInput = document.getElementById("rent");
-  const rent = parseFloat(rentInput.value);
-  const clothInput = document.getElementById("cloth");
-  const cloth = parseFloat(clothInput.value);
-
-  const expense = document.getElementById("expense");
-  const expenseNumber = parseFloat(expense.innerText);
-  const totalExpense = food + rent + cloth;
-  expense.innerText = totalExpense; */
 });
-//saving part
+//Saving part
 document.getElementById("save-btn").addEventListener("click", function () {
   const saveInput = document.getElementById("percent-input");
   const inputNumber = parseFloat(saveInput.value);
-  if (inputNumber > 0) {
+  if (inputNumber >= 0) {
     const paymentInput = document.getElementById("payment");
     const payment = parseFloat(paymentInput.value);
     const countPercent = payment * (inputNumber / 100);
@@ -74,5 +62,10 @@ document.getElementById("save-btn").addEventListener("click", function () {
     const remain = parseFloat(remainingBalance.innerText);
     const netBalance = calculation() - saveTotal.innerText;
     remainingBalance.innerText = netBalance;
+    const error = document.getElementById("error");
+    error.style.display = "none";
+  } else {
+    const error = document.getElementById("error");
+    error.style.display = "block";
   }
 });
